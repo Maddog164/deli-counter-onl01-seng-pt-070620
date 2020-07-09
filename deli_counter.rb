6eb5f9ada7c4ee1af13e058038e6a8562b00101e
Denise Henderson <denise@henderson3.us>
2020-07-09 18:54:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,27 +6,18 @@ def line(array)
     arraycount += 1
   end
   if arraycount == 0
-    #binding.pry
     message = "The line is currently empty."
   else
-    #binding.pry
     message = "The line is currently:"
-    #binding.pry
     counter = 0
-    #binding.pry
     while counter < arraycount
-      #binding.pry
       number = counter+1
       name = array[counter]
-      #binding.pry
       msgline =  " #{number}. " + "#{name}"
       message = message + msgline
-      #binding.pry
       counter += 1
-      #binding.pry
     end
    end
-   #binding.pry
    puts message
    return message
 end
