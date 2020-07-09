80dd1281a8163ad5642c7a1588b13ba1b8337e89
Denise Henderson <denise@henderson3.us>
2020-07-09 18:34:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+  arraycount = 0
+  array.each do |name|
+    arraycount += 1
+  end
+  if arraycount == 0
+    binding.pry
+    puts "The line is currently empty."
+  else
+    #binding.pry
+    puts "The line is currently: "
+    #binding.pry
+    counter = 0
+    #binding.pry
+    while counter < arraycount
+      binding.pry
+      number = counter+1
+      name = array[counter]
+      #binding.pry
+      msgline =  "#{number}. " + "#{name} "
+      puts msgline
+      #binding.pry
+      counter += 1
+      binding.pry
+    end
+   end
+end
